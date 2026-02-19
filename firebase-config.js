d39dd12ed81eaa4b5bf45fd51085b2f0bcdd4ca7
MoseMan-creator <david.moseley7503@gmail.com>
2026-02-19 08:38:42 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBtyCSCNiIXSEkLW3j-k6mjC1oXHGTTUbk",
+  authDomain: "calproduct-53dc0.firebaseapp.com",
+  projectId: "calproduct-53dc0",
+  storageBucket: "calproduct-53dc0.firebasestorage.app",
+  messagingSenderId: "377170422640",
+  appId: "1:377170422640:web:c7092164d15640406b3db5",
+  measurementId: "G-3MFCRT3XWN"
 };
 
 // Initialize Firebase
